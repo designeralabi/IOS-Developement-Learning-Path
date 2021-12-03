02f89b683246316aef1ce51d85a7ef9d5771f614
designeralabi <bdlkrmalabi@gmail.com>
2021-12-03 01:39:59 -0800

    added functionality controls

--- TwoButton/TwoButton/ViewController.swift
+++ TwoButton/TwoButton/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+    @IBOutlet var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func setButtonTapped(_ sender: UIButton) {
+        label.text = textField.text
+    }
+    
+    @IBAction func clearButtonTapped(_ sender: UIButton) {
+        textField.text = ""
+        label.text = ""
+    }
+    
 }
 
