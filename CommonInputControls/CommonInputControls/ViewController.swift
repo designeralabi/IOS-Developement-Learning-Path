4d4f045209f5a90024996c23c5ebd95d3ae06ce8
designeralabi <bdlkrmalabi@gmail.com>
2021-12-03 01:17:44 -0800

    added twoButtonApp Project

--- CommonInputControls/CommonInputControls/ViewController.swift
+++ CommonInputControls/CommonInputControls/ViewController.swift
@@ -9,28 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var toggleSwitch: UISwitch!
+    @IBOutlet var toggleSlider: UISlider!
+    @IBOutlet var buttonNew: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // code didn't work
+//    buttonNew.addTarget(self, action: #selector(buttonTapped(_:)),
+//       for: .touchUpInside)
+    
     // return value based on condition
     @IBAction func switchTapped(_ sender: UISwitch) {
-        if sender.isOn{
-            print("The switch is on!")
-        }else{
-            print("The switch is off.")
-        }
+        //do nothing
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         print("Click here was tapped")
+        
+        if toggleSwitch.isOn{
+            print("The switch is on!")
+        }else{
+            print("The switch is off.")
+        }
+        
+        print("The slide is set to \(toggleSlider.value)")
+    }
+    
+    @IBAction func respondToTapGesture(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: view)
+        print(location)
     }
     
     @IBAction func textChanged(_ sender: UITextField) {
         print(sender.text!)
         
     }
+    
     // this print the user text when enter key is pressed
     @IBAction func keyboardReturnKeyTapped(_ sender: UITextField) {
         print(sender.text!)
@@ -39,5 +57,6 @@ class ViewController: UIViewController {
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         print(sender.value)
     }
+    
 }
 
