dbad5956916e16bb56381681c70b5272d4d1d846
designeralabi <bdlkrmalabi@gmail.com>
2021-12-12 17:14:48 -0800

    created IBOutlet for all object for code reference

--- applePie/applePie/ViewController.swift
+++ applePie/applePie/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
